520c2ef4859c5f824aba879e75da2e8bea2f1a37
Alexander Eimer <alexander.eimer@gmail.com>
2017-11-16 17:08:26 +0100

    Adds desired functions

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,22 @@ gulp.task(RELEASE, function () {
     .pipe()
     .pipe();
 });
+
+
+// Tasks:
+// - BUMP_VERSION
+//   - git stash
+//   - git checkout develop
+//   - git checkout -b version/bump-x.x.x
+//   - bump version in all files
+//   - git commit
+//   - git push
+//   - github open PR to develop
+//   - git unstash
+// - RELEASE
+//  - search for pr from develop to master
+//  - show which one are found / select one
+//  - check if version does not exists already and is in every file the same
+//  - merge pr (if approved)
+//  - ask for title and description of release
+//  - create new release
